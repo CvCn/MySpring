8385305eb8e15ebf6443d0224b4b0eb6a0c5570e
CvCn <876247638@qq.com>
2019-05-09 18:31:58 +0800

    添加web容器

---
+++ src/main/java/com/moxiaowei/webcontroller/MySpringLister.java
@@ -0,0 +1,17 @@
+package com.moxiaowei.webcontroller;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MySpringLister implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
